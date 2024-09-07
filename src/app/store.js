40664bf4c5ca96e8c7a8b27c28f287b3449d4a4e
Hugo <119013233+hugo-ste@users.noreply.github.com>
2024-09-07 15:23:04 -0400

    Add topicsSlice reducer to store.js. (Task 6)

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-// import reducers
+import topicsReducer from "../features/topics/topicsSlice";
 
 export default configureStore({
   reducer: {
-    
+    topics: topicsReducer
   },
 });

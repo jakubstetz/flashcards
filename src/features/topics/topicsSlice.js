19fec33e4c5cc90013ea42e062f27b23a29968bd
Hugo <119013233+hugo-ste@users.noreply.github.com>
2024-09-07 15:21:04 -0400

    Create topicsSlice.js. (Task 5)

---
+++ src/features/topics/topicsSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// Slice Object
+export const topicsSlice = createSlice({
+  name: 'topics',
+  initialState: {topics: {}},
+  reducers: {
+    addTopic: (state, action) => {
+      const {id, name, icon} = action.payload;
+      state.topics[id] = {id, name, icon, quizIds: []};
+    }
+  }
+});
+
+// Selector
+export const selectTopics = state => state.topics;
+
+// Exports from Slice
+export const { addTopic } = topicsSlice.actions;
+export default topicsSlice.reducer;
